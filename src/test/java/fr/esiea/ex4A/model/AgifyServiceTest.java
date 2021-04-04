212f2c720a601e9d2996b6d966847224ebf3b854
eric Mouss <Manfoumbieric@gmail.com>
2021-04-04 14:58:51 +0200

    changement cache

---
+++ src/test/java/fr/esiea/ex4A/model/AgifyServiceTest.java
@@ -0,0 +1,19 @@
+package fr.esiea.ex4A.model;
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AgifyServiceTest {
+
+    @Test
+    void nouveauInscrit() {
+        //TODO
+    }
+
+    @Test
+    void getMatches() {
+        //TODO
+    }
+}
